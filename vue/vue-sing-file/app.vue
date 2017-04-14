e0a2018e0e24ccc51c86117f1cd778d38e99ba04
心太软 <1642903202@qq.com>
2017-04-14 10:52:53 +0800

    vue 包install

--- vue/vue-sing-file/app.vue
+++ vue/vue-sing-file/app.vue
@@ -1,22 +1,27 @@
 <template>
-    <!-- 在vue1.0的时候，此时结构随便你写，在2.0中，只能以一个根节点的形式来写 -->
     <div class="tmpl">
         {{text}}
     </div>
 </template>
+
 <script>
-//固定格式
-export default {
-    data: function () {
-        //在单文件的方式中，data是一个函数，和库的方式是对象，不一样
-        return {
-            text: 'hello vue!'
+// 固定格式
+    export default {
+        // data: function () {
+        //     // 在单文件的方式中，data是一个函数，和库的方式是对象，不一样
+        //     return {
+        //         text: 'hello vue!'
+        //     }
+        // }
+        data() {
+            return {
+                text: 'hello vue'
+            }
         }
     }
-}
 </script>
 <style scoped>
-.tmpl {
-    background-color: blue;
-}
+    .tmpl {
+        background-color: blue;
+    }
 </style>
--- vue/vue-sing-file/main.js
+++ vue/vue-sing-file/main.js
@@ -1,5 +1,6 @@
-//1:引入Vue实例对象
-import Vue from 'vue'; //逐级向上查找node_modules/vue文件夹的入口
+// 1:引入Vue实例对象
+// 逐级向上查找node_modules/vue文件夹的入口
+import Vue from 'vue';
 
 import App from './app.vue';
 
@@ -10,7 +11,8 @@ import App from './app.vue';
 new Vue({
     el: '#app',
     // data:这个属性留给app.vue来使用
-    render: function(creater) { //创建元素的对象，是一个函数
-        return creater(App); //创建一个元素，并且返回，让vue知道，我们要render什么东西
-    }
+    // render: function(creater) { //创建元素的对象，是一个函数
+    //     return creater(App); //创建一个元素，并且返回，让vue知道，我们要render什么东西
+    // }
+    render: c => c(App)
 });
