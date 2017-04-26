b09221447e3a6a95fd3cd1d3a10ab911a9e46c84
心太软 <1642903202@qq.com>
2017-04-26 23:30:07 +0800

    完善细节

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-
+// 
 function reverse(arr) {
     var temp = [];
     for (var i = arr.length - 1; i >= 0; i--) {
@@ -24,41 +24,72 @@ function sort(arr) {
     }
     return arr;
 }
+Array.prototype.unique = function () {
+    var obj = {}
+    var res = []
+    for (var i = 0; i < this.length; i++) {
+        if (!obj[this[i]]) {
+            res.push(this[i])
+            obj[this[i]] = 1
+        }
+    }
+    return res;
+}
+    // var length = 10;
+    // function fn() {
+    //     console.log(this.length);
+    // }
+    // var obj = {
+    //     length: 5,
+    //     method: function (f) {
+    //          f();
 
-// var length = 10;
-// function fn() {
-//     console.log(this.length);
-// }
-// var obj = {
-//     length: 5,
-//     method: function (f) {
-//          f();
+    //         // arguments[0]();
+    //         // arguments[0].call(this);
+    //     }
+    // }
+    // obj.method(fn);
 
-//         // arguments[0]();
-//         // arguments[0].call(this);
-//     }
-// }
-// obj.method(fn);
+    // function name(x,y){
+    //     console.log(x+y)
+    // }
+    // var name=(x,y)=>x+y;
+    // console.log(name(1,2))
 
-// function name(x,y){
-//     console.log(x+y)
-// }
-// var name=(x,y)=>x+y;
-// console.log(name(1,2))
+    // var str = "123456";
+    // console.log(str.split("").reverse().join(""))
 
-// var str = "123456";
-// console.log(str.split("").reverse().join(""))
+    // console.log([...str].reverse().join(""));
 
-// console.log([...str].reverse().join(""));
+    // var arr =[{id:2,name:"wokao"},{id:6,name:"sb"}]
+    // // let str
+    // // var ids=[]
+    // // arr.forEach((item)=>{
+    // //     ids.push(item.id)
+    // // })
+    // // console.log(str=ids.join(","))
+    // arr.splice(0,1)
+    // console.log(arr)
 
-var arr =[{id:2,name:"wokao"},{id:6,name:"sb"}]
-// let str
-// var ids=[]
-// arr.forEach((item)=>{
-//     ids.push(item.id)
-// })
-// console.log(str=ids.join(","))
-arr.splice(0,1)
-console.log(arr)
+    //  var arr1 = [1, 2, 3, 4, 5];
+    //  var arr2 = [1, 2, 5, 7, 9];
+    //  var res = []
+    //  var obj = {}
+    // arr1.concat(arr2).forEach(function (p) {
+    //     if (!obj[p]) {
+    //         obj[p] = 1
+    //     }else{
+    //         res.push(p)
+    //     }
+    // })
+    //  console.log(res) ;
 
+    //从性能优化角度来考虑，这题用着个方式，一次循环就可以搞定，filter, 和indexof 两次循环了
 
+    (function () {
+        var i = 0;
+        return function () {
+            i++
+            console.log(i)
+        }()
+    }())
